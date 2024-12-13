bfe7ee0750df9e357ad596b842d6f1837f895342
Great-special <72235177+Great-special@users.noreply.github.com>
2024-12-13 01:38:41 +0100

    Update settings.py

--- PersonalPortfolio/settings.py
+++ PersonalPortfolio/settings.py
@@ -134,7 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # The location of the new static files 
-STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles", 'static')
+STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles_build", 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
