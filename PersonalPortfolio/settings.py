7ce4a3299f0f66d27bfdb8af94cf88405f2a3203
Great-special <72235177+Great-special@users.noreply.github.com>
2024-12-13 01:27:14 +0100

    Update settings.py

--- PersonalPortfolio/settings.py
+++ PersonalPortfolio/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = SECRETS
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['godisgreat-nwachukwu.up.railway.app', '127.0.0.1', '3zk-devoted-geiger.circumeo-apps.net']
-CSRF_TRUSTED_ORIGINS = ['https://godisgreat-nwachukwu.up.railway.app']
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*']
 
 
 # Application definition
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'PersonalPortfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -131,10 +131,10 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # The location of the new static files 
-STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles", 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
